37faef90412d452d6ff450da5a28288bc6f7e59a
truongquocduy <quocduy13579113@gmail.com>
2022-11-03 15:31:11 +0700

    update file connect db

--- Model/connect.php
+++ Model/connect.php
@@ -4,9 +4,9 @@ class connect
 	var $db=null;
 	public function __construct() 
 	{
-		$dsn='mysql:host=localhost;dbname=school';
-		$user='root';
-		$pass='';
+		$dsn='mysql:host=sql12.freesqldatabase.com;dbname=sql12537854';
+		$user='sql12537854';
+		$pass='4vsTQn4qTf';
 		$this->db=new PDO($dsn,$user,$pass,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
 	}
 //	https://megacode.vn/files/view/php-phuong-thuc-getinstance-la-gi-va-tai-sao-dung-getinstance-4922.html
